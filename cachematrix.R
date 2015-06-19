311aa2d8aeab57c5ea0443e9b906accd839601d2
TugolukovaAlina <tugolukovaalina@gmail.com>
2015-06-19 12:07:10 +0200

    v1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creates a special matrix that can store its inverse matrix 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(M = matrix()) {
 
+     # stores inverse matrix of M if it has been computed
+     inverseM <- NULL
+
+     set <- function(y) {
+          M <<- y
+          # set inverse matrix to null if matrix M has been modified
+          inverseM <<- NULL
+     }
+     get <- function() M
+     setInverseMatrix <- function(invM) inverseM <<- invM
+     getInverseMatrix <- function() inverseM
+
+     list(set = set, get = get,
+          setInverseMatrix = setInverseMatrix,
+          getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of the matrix returned by `makeCacheMatrix` above. 
+## if inverse matrix has already been computed returns cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(M) {
+     
+     inverseM <- M$getInverseMatrix()
+     
+     # check if matrix has not changed and inverse matrix has already been counted
+     # then return already computed inverse matrix
+     if( !is.null(inverseM)) {
+          message("getting cached data")
+          return(inverseM)
+     }
+     
+     # compute inverse matrix of M
+     data <- M$get()
+     inverseM <- solve(data)
+     M$setInverseMatrix(inverseM)
+     inverseM
 }
